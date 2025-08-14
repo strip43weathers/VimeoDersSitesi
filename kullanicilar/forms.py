4156d18ce3e1dd5d074f7087f66edf677279ab37
furkan aygun <furkanaygun2004@gmail.com>
2025-08-14 23:48:52 +0300

    Marka adı Romingo olarak güncellendi ve giriş formu Türkçeleştirildi

--- kullanicilar/forms.py
+++ kullanicilar/forms.py
@@ -1,10 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class KayitFormu(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Lütfen geçerli bir e-posta adresi girin.")
-
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
+
+class CustomAuthenticationForm(AuthenticationForm):
+    # Bu __init__ metodu, formun alanlarını Türkçeleştirmek için eklendi.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = "Kullanıcı Adı"
+        self.fields['password'].label = "Parola"
+
+    error_messages = {
+        'invalid_login': (
+            "Kullanıcı adı veya parolanız hatalı. "
+            "Not: Hesabınız henüz yönetici tarafından onaylanmamış olabilir."
+        ),
+        'inactive': ("Bu hesap aktif değil."),
+    }
