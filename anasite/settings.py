ce1ea0eed103be145bc8be814d8ee5e3778bbf79
furkan aygun <furkanaygun2004@gmail.com>
2025-08-14 20:59:23 +0300

    ALLOWED_HOSTS ayarı eklendi

--- anasite/settings.py
+++ anasite/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# anasite/settings.py
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 
 # Application definition
