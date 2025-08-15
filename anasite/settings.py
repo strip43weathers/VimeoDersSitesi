b48f373540501e8b29f53cfd511f40bb2664e5d8
furkan aygun <furkanaygun2004@gmail.com>
2025-08-15 23:32:58 +0300

    PostgreSQL baglantisi kuruldu

--- anasite/settings.py
+++ anasite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from decouple import config
 from pathlib import Path
 
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'anasite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
