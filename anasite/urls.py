e6b79c0a4a416256be0ae0ed5cfae8a650519a5e
furkan aygun <furkanaygun2004@gmail.com>
2025-08-15 18:59:27 +0300

    Ana sayfa için yönlendirme eklendi

--- anasite/urls.py
+++ anasite/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include # include'ü import etmeyi unutma
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+# BU SATIRI EKLEYİN
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dersler/', include('dersler.urls')), # Bu satırı ekliyoruz
-    path('hesaplar/', include('django.contrib.auth.urls')),
-    path('kullanicilar/', include('kullanicilar.urls')), # Bu satırı ekliyoruz
-]
+                  # BU SATIRI EKLEYİN: Ana adrese gelenleri /hesaplar/login/ adresine yönlendirir.
+                  path('', RedirectView.as_view(url='/hesaplar/login/', permanent=True)),
+
+                  path('admin/', admin.site.urls),
+                  path('dersler/', include('dersler.urls')),
+                  path('hesaplar/', include('hesaplar.urls')),
+                  path('kullanicilar/', include('kullanicilar.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
