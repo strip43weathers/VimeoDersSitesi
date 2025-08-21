05632c7a36f4afbc8164dab7606d22fd47f9b143
furkan aygun <furkanaygun2004@gmail.com>
2025-08-21 18:04:00 +0300

    as

--- anasite/settings.py
+++ anasite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sitemaps',
     'dersler',
     'kullanicilar',
     'crispy_bootstrap5',
--- anasite/urls.py
+++ anasite/urls.py
@@ -1,57 +1,33 @@
-# anasite/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
 from dersler import views as dersler_views
-
-# Sitemap için gerekli import'lar
-from django.contrib.sitemaps.views import sitemap
-from .sitemaps import KursSitemap, SayfaSitemap, StaticViewSitemap # OyunSitemap'i sildik
 
 from django.contrib.auth import views as auth_views
 from kullanicilar.forms import CustomPasswordResetForm
 
-# sitemaps sözlüğünü oluşturun
-sitemaps = {
-    'kurslar': KursSitemap,
-    'sayfalar': SayfaSitemap,
-    'static': StaticViewSitemap,
-}
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Sitemap ve robots.txt URL'leri
-    path(
-        "sitemap.xml",
-        sitemap,
-        {"sitemaps": sitemaps},
-        name="django.contrib.sitemaps.views.sitemap",
-    ),
-    path(
-        "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
-    ),
-
     path('', dersler_views.anasayfa_view, name='anasayfa'),
 
+    # ÖZEL URL'LERİ GENEL OLANLARDAN ÖNCEYE ALIYORUZ
     path('kurslar/', include('dersler.urls')),
     path('oyunlar/', include('oyunlar.urls')),
     path('kullanicilar/', include('kullanicilar.urls')),
 
     path(
-        'hesaplar/password_reset/',
+        'hesaplar/password_reset/', 
         auth_views.PasswordResetView.as_view(
             form_class=CustomPasswordResetForm,
             template_name='registration/password_reset_form.html'
-        ),
+            ), 
         name='password_reset'
     ),
     path('hesaplar/', include('django.contrib.auth.urls')),
 
+    # EN GENEL URL'İ (TÜM STATİK SAYFALARI KAPSAYAN) EN SONA KOYUYORUZ
     path('', include('sayfalar.urls')),
 ]
 
