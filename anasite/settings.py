d29a3a07e10e2db40d0de068b97fdabe44ae0fc3
furkan aygun <furkanaygun2004@gmail.com>
2025-10-06 19:03:04 +0300

    geri alma

--- anasite/settings.py
+++ anasite/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'anasite.urls'
--- anasite/wsgi.py
+++ anasite/wsgi.py
@@ -1,17 +1,9 @@
-# anasite/wsgi.py
+# anasite/wsgi.py (Orijinal Hali)
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anasite.settings')
 
-# Temel Django uygulamasını al
 application = get_wsgi_application()
-
-# WhiteNoise ile sarmala: Önce statik dosyalar için
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-
-# WhiteNoise'a medya dosyalarını da ekle
-application.add_files(settings.MEDIA_ROOT, prefix='media/')
