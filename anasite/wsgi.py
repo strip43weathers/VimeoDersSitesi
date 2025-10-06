95c0dd5bc8a032912812207302fd03c23e350301
furkan aygun <furkanaygun2004@gmail.com>
2025-10-06 18:03:06 +0300

    foto

--- anasite/wsgi.py
+++ anasite/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.conf import settings
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anasite.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
