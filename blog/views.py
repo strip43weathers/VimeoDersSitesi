dc807f108028359ba38ff592972746fdde0c619f
furkan aygun <furkanaygun2004@gmail.com>
2025-10-06 16:41:39 +0300

    blog

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,20 @@
 from django.shortcuts import render
 from .models import BlogYazisi
 from sayfalar.models import RehberVideo
+from django.utils.text import Truncator
+
 
 def blog_listesi_view(request):
     yazilar = BlogYazisi.objects.all()
-    # Sadece blog sayfası için aktif olan videoyu çek
-    rehber_video = RehberVideo.objects.filter(aktif=True, sayfa='blog').first()
+
+    for yazi in yazilar:
+        yazi.ozet = Truncator(yazi.icerik).words(30, html=True, truncate='...')
+
+    # 'blog' sayfası için aktif olan TÜM videoları çek
+    rehber_videolar = RehberVideo.objects.filter(aktif=True, sayfa='blog')
 
     context = {
         'yazilar': yazilar,
-        'rehber_video': rehber_video
+        'rehber_videolar': rehber_videolar  # Değişken adını çoğul yapıyoruz
     }
     return render(request, 'blog/blog_listesi.html', context)
